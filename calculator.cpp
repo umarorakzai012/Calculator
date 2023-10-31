d21b4aba99f88212548f37a70405e76c50fedcf0
umarorakzai012 <umarorakzai012@gmail.com>
2023-10-31 18:21:42 +0500

    added -, *, / as well, using switch case

--- calculator.cpp
+++ calculator.cpp
@@ -10,7 +10,32 @@ int main(){
     cout << "Enter Number 2: ";
     cin >> num2;
 
-    cout << "Result of Addition: " << (num1 + num2);
+    char operand;
+    cout << "+, -, *, /: ";
+    cin >> operand;
+
+    switch (operand)
+    {
+    case '+':
+        cout << "Result of Addition: " << (num1 + num2);
+        break;
+
+    case '-':
+        cout << "Result of Subtraction: " << (num1 - num2);
+        break;
+
+    case '*':
+        cout << "Result of Multiplication: " << (num1 * num2);
+        break;
+
+    case '/':
+        cout << "Result of Division: " << (num1 / num2);
+        break;
+
+    default:
+        break;
+    }
+
 
     return 0;
 }
