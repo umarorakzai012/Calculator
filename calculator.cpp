8bafa7da849e66b7f3d3b6cb5da37d359dee7a4b
umarorakzai012 <umarorakzai012@gmail.com>
2023-10-31 18:32:49 +0500

    added while loop for many calculations

--- calculator.cpp
+++ calculator.cpp
@@ -4,50 +4,53 @@ using namespace std;
 
 int main(){
     int num1, num2;
-
-    cout << "Enter Number 1: ";
-    cin >> num1;
-    cout << "Enter Number 2: ";
-    cin >> num2;
-
     char operand;
-    cout << "+, -, *, /: ";
-    cin >> operand;
 
-    switch (operand)
-    {
-    case '+':
-        cout << "Result of Addition: " << (num1 + num2);
-        break;
+    do{
+        cout << "\n\nEnter Number 1: ";
+        cin >> num1;
+        cout << "Enter Number 2: ";
+        cin >> num2;
 
-    case '-':
-        cout << "Result of Subtraction: " << (num1 - num2);
-        break;
+        cout << "+, -, *, / or q(quit): ";
+        cin >> operand;
 
-    case '*':
-        cout << "Result of Multiplication: " << (num1 * num2);
-        break;
-
-    case '/':
-        try
+        switch (operand)
         {
-            if(num2 == 0){
-                throw exception();
+        case '+':
+            cout << "Result of Addition: " << (num1 + num2);
+            break;
+
+        case '-':
+            cout << "Result of Subtraction: " << (num1 - num2);
+            break;
+
+        case '*':
+            cout << "Result of Multiplication: " << (num1 * num2);
+            break;
+
+        case '/':
+            try
+            {
+                if(num2 == 0){
+                    throw exception();
+                }
+                cout << "Result of Division: " << (num1 / num2);
             }
-            cout << "Result of Division: " << (num1 / num2);
-        }
-        catch(const exception& e)
-        {
-            cerr << "Cannot divide by zero" << '\n';
-        }
+            catch(const exception& e)
+            {
+                cerr << "Cannot divide by zero" << '\n';
+            }
+            break;
         
-        break;
+        case 'q':
+            break;
 
-    default:
-        cout << "Choose correct option please!";
-        break;
-    }
-
+        default:
+            cout << "Choose correct option please!";
+            break;
+        }
+    }while(operand != 'q');
 
     return 0;
 }
