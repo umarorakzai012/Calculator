00bd234c6ebb592b8f75b7fc1a6699f904739472
umarorakzai012 <umarorakzai012@gmail.com>
2023-10-31 18:16:53 +0500

    initial commit and addition part done

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cout << "Enter Number 1: ";
+    cin >> num1;
+    cout << "Enter Number 2: ";
+    cin >> num2;
+
+    cout << "Result of Addition: " << (num1 + num2);
+
+    return 0;
+}
